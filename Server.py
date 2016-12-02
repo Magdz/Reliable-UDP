6200ee8b7c6afe5ce945174a46bf960554a2df69
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-12-02 14:41:52 +0200

    Create Server module #9

--- Server.py
+++ Server.py
@@ -1,1 +1,24 @@
 # here goes the properties and the functions of the Server as an Object
+
+import socket
+import sys
+
+class Server:
+	ip = None
+	port = None
+	connection = None
+
+	def __init__(self, ip, port):
+		self.ip = ip
+		self.port = port
+		self.initializeSocket()
+
+	def initializeSocket(self):
+		try:
+			# UDP Socket Connection
+			self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			self.connection.bind((self.ip, self.port))
+			print "Server socket connection initialized"
+		except Exception, e:
+			print "Server socket connection faild: " + e
+			sys.exit()
