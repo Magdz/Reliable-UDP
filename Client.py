82aae59e1958011757ef57bc289e5b777e4f0499
Dina Nashaat <dina.nashaat@gmail.com>
2016-12-03 04:35:41 +0200

    Create Client module #11

--- Client.py
+++ Client.py
@@ -1,1 +1,32 @@
-# here goes the properties and the functions of the Client as an Object
+import socket
+import sys
+
+class Client(object):
+    """
+    Defines the Client properties and methods
+    """
+    __hostname = None
+    __port = None
+    __clientsocket = None
+
+    def __init__(self, hostname, port):
+        self.__ip = hostname
+        self.__port = port
+        self.initialize_socket()
+
+    def initialize_socket(self):
+        """
+        Initialize Client Socket
+        """
+        try:
+            self.__clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print 'Client Socket Created successfully.'
+        except socket.error:
+            print 'Failed to create a Client Socket'
+            sys.exit()
+    def close_socket(self):
+        """
+        Close opened socket
+        """
+        self.__clientsocket.close()
+        
