593613d5bda4854ea27c5a00e25231c0a6a64be3
Dina Nashaat <dina.nashaat@gmail.com>
2016-12-04 22:42:45 +0200

    Format files to python standard format

--- Datagram.py
+++ Datagram.py
@@ -1,25 +1,29 @@
-import sys
 import random
 
-class Datagram:
-	ipFrom = None
-	portFrom = None
-	ipTo = None
-	portTo = None
-	packet = None
-	__PLP = None
-	__Probability = 0.1	# Higher Probability means Higher losses rate
+class Datagram(object):
+    ip_from = None
+    port_from = None
+    ip_to = None
+    port_to = None
+    packet = None
+    __plp = None
+    __probability = 0.1	# Higher probability means Higher losses rate
 
-	def __init__(self, ipFrom, portFrom, ipTo, portTo, packet):
-		self.ipFrom = ipFrom
-		self.portFrom = portFrom
-		self.ipTo = ipTo
-		self.portTo = portTo
-		self.packet = packet
-		self.setProbability()
+    def __init__(self, ip_from, port_from, ip_to, port_to, packet):
+        self.ip_from = ip_from
+        self.port_from = port_from
+        self.ip_to = ip_to
+        self.port_to = port_to
+        self.packet = packet
+        #self.setprobability()
+    def setprobability(self):
+        """
+		Simulate the Packet Loss with defining a random probability
+		"""
+        self.__plp = int(random.uniform(0, 1) + self.__probability)
 
-	def setProbability(self):
-		self.__PLP = int(random.uniform(0, 1) + self.__Probability)
+    def isLost(self):
+        """
 
-	def isLost(self):
-		return self.__PLP
+		"""
+        return self.__plp
--- MainServer.py
+++ MainServer.py
@@ -1,107 +1,105 @@
 """
-Main logic for running the server goes here.
+Main logic for running the SERVER goes here.
 """
 
 import sys
 import socket
 import cPickle as pickle
-import os
 from Server import Server
 from SenderHelper import SenderHelper
 from DataPacket import DataPacket
 from AckPacket import AckPacket
 from Datagram import Datagram
 
-myIp = '127.0.0.1'
-myPort = 8888
-seqNo = 0
-ackNo = 0
-checkSum = 0
+#Connection Details and Function Params
+HOSTNAME = '127.0.0.1'
+PORT = 8888
+SEQNO = 0
+ACKNO = 0
+CHECKSUM = 0
 
-# Starting Server
-server = Server(myIp, myPort)
-print "Server Started"
+# Starting SERVER
+SERVER = Server(HOSTNAME, PORT)
+print "SERVER Started"
 
 # Waiting until a Request is received
-"""
-
-"""
 WAITTOCONNECT = True
 while WAITTOCONNECT:
-	try:
-		recvRequest = server.connection.recvfrom(1024)
-		WAITTOCONNECT = False	
-	except socket.timeout:
-		pass
+    try:
+        RECVREQUEST = SERVER.connection.recvfrom(1024)
+        WAITTOCONNECT = False
+    except socket.timeout:
+        pass
 
-# INFO = recvRequest[1].split('\n')
-REQUESTS = recvRequest[0].split('\n')
+#Fetch Requests variables
+REQUESTS = RECVREQUEST[0].split('\n')
+IP = RECVREQUEST[1][0]
+FILENAME = REQUESTS[3]
+PORT = RECVREQUEST[1][1]
 
-ip = recvRequest[1][0]
-filename = REQUESTS[3]
+#Print Information given from Client
+print "Received a request for " + str(FILENAME)
+print "Request is received from IP: " + str(IP) + " from PORT: " + str(PORT)
 
-print "Received a request for " + str(filename)
-port = recvRequest[1][1]
-print "Request is received from IP: " + str(ip) + " from Port: " + str(port)
-
-# Check of file existance and send response
-if(SenderHelper.fileExists(filename)):
-	sendResponse = "200 OK"
+# Check if file exists and send response
+if SenderHelper.fileExists(FILENAME):
+    SENDRESPONSE = "200 OK"
 else:
-	sendResponse = "404 Not Found"
-server.connection.sendto(sendResponse, (myIp, port))
-print "Sending Response: " + sendResponse
+    SENDRESPONSE = "404 Not Found"
+SERVER.connection.sendto(SENDRESPONSE, (HOSTNAME, PORT))
+print "Sending Response: " + SENDRESPONSE
 
 
-# Split the file into chunks
-chunks = SenderHelper.createChunks(filename)
-print "Chunks Created"
-server.connection.sendto(str(len(chunks)), (myIp, port))
-print "Size of chunks is: "  + str(sys.getsizeof(chunks))
-print "Length of chunks is: " + str(len(chunks))
+# Split the file into CHUNKS
+CHUNKS = SenderHelper.createChunks(FILENAME)
+print "CHUNKS Created"
+SERVER.connection.sendto(str(len(CHUNKS)), (HOSTNAME, PORT))
+print "Size of CHUNKS is: "  + str(sys.getsizeof(CHUNKS))
+print "Length of CHUNKS is: " + str(len(CHUNKS))
 
 # Start Processing
-#index = 0
-for chunk in chunks:
-	print "\n"
-	sendPacket = DataPacket(chunk, seqNo, checkSum)
-	sendGram = Datagram(myIp, myPort, ip, port, sendPacket)
-	sendMsg = pickle.dumps(sendGram, -1)
-	ackNo = seqNo
-	while True:
-		sendGram.setProbability()
-		if not sendGram.isLost():
-			server.connection.sendto(sendMsg, (sendGram.ipTo, sendGram.portTo))
-		print "Sending Packet #" + str(seqNo)
-		print "Timer Started"
-		seqNo = ackNo
-		# start timer
-		print "Waiting for Ack #" + str(ackNo)
-		try:
-			recvMsg = server.connection.recvfrom(1024)
-		except socket.timeout:
-			print "Timeout"
-			continue
-		recvGram = pickle.loads(recvMsg[0])
-		recvPacket = recvGram #.packet
-		if(type(recvPacket) is AckPacket):
-			print "Received Ack #" + str(recvPacket.ackNo)
-			if(recvPacket.ackNo == ackNo):
-				seqNo = 1 - seqNo
-				# stop timer
-				print "Timer Stopped"
-				break
-			else:
-				print "Unexpected Ack"
-				# stop timer
-				print "Timer Stopped"
-				print "Resending Packet #" + str(seqNo)
-		else:
-			print "Received a corrupted packet"
-			# stop timer
-			print "Timer Stopped"
-			print "Resending Packet #" + str(seqNo)
-		print "\n"
+for chunk in CHUNKS:
+    print "\n"
+    sendPacket = DataPacket(chunk, SEQNO, CHECKSUM)
+    sendGram = Datagram(HOSTNAME, PORT, IP, PORT, sendPacket)
+    sendMsg = pickle.dumps(sendGram, -1)
+    ACKNO = SEQNO
+	#
+    while True:
+        #sendGram.setprobability()
+        if not sendGram.isLost():
+            SERVER.connection.sendto(sendMsg, (sendGram.ip_to, sendGram.port_to))
+        print "Sending Packet #" + str(SEQNO)
+        print "Timer Started"
+        SEQNO = ACKNO
+
+		# Start timer
+        print "Waiting for Ack #" + str(ACKNO)
+        try:
+            recvMsg = SERVER.connection.recvfrom(1024)
+        except socket.timeout:
+            print "Timeout"
+            continue
+        recvGram = pickle.loads(recvMsg[0])
+        recvPacket = recvGram
+        if isinstance(recvPacket, AckPacket):
+            print "Received Ack #" + str(recvPacket.ackNo)
+            if recvPacket.ackNo == ACKNO:
+                SEQNO = 1 - SEQNO
+				# Stop timer
+                print "Timer Stopped"
+                break
+            else:
+                print "Unexpected Ack"
+        		# Stop timer
+                print "Timer Stopped"
+                print "Resending Packet #" + str(SEQNO)
+        else:
+            print "Received a corrupted packet"
+			# Stop timer
+            print "Timer Stopped"
+            print "Resending Packet #" + str(SEQNO)
+        print "\n"
 
 print "All Packets are sent successfully"
-print "Closing the server"
+print "Closing the SERVER"
